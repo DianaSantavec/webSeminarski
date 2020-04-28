e47920ba0c42a2711f1895b59041af3b30b8fc21
DianaSantavec <diana.santavec@gmail.com>
2020-04-28 14:55:07 +0200

    Initial commit

---
+++ Models/UciniciClass.cs
@@ -0,0 +1,9 @@
+namespace webSeminarski.Models{
+
+    public class Ucenici{
+        public int id {get;set;}
+        public string ime {get;set;}
+        public string prezime {get;set;}
+        public float prosek {get;set;}
+    }
+}
